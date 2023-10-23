9d82ae6f05d4bcada3caad154ad8fe4c1152bee7
Milton Junior <milton.junior@defensoria.mg.def.br>
2023-10-22 21:13:24 -0300

    primeiro commit

---
+++ src/main/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./main.css"
+
+export default () =>
+    <div className="main">
+        <div class="card">
+            <h1>Adriana Sauter Cabral</h1>
+            <p>Minha querida Adriana,<br />
+                Escrevo estas palavras com um coração grato e cheio de amor, porque quero expressar o quanto você significa para mim e como a sua presença na minha vida tem sido um presente divino.
+
+                Desde o momento em que nos conhecemos no Facebook em maio deste ano, a minha vida mudou de uma forma que eu nunca poderia ter imaginado. Foi como se Deus tivesse decidido unir os nossos caminhos, e desde então, tenho experimentado uma alegria e felicidade que eu jamais poderia ter previsto.
+
+                Cada vez que vou para Santa Catarina, onde você mora, sinto como se meu mundo se enchesse de felicidade. A sua cidade se tornou um lugar especial para mim, não apenas por sua beleza natural, mas também porque é onde você está. As nossas conversas, os nossos passeios, as nossas risadas e até mesmo os nossos silêncios compartilhados têm sido momentos inesquecíveis que guardo no meu coração.
+
+                Você é a razão do meu sorriso, da minha esperança e da minha gratidão diária a Deus. Cada dia ao seu lado é uma bênção, e a cada momento que compartilhamos, sinto que estou mais perto de encontrar o meu verdadeiro lar, que é o lugar onde o seu coração está.
+
+                Adriana, quero que saiba que o amor que sinto por você cresce a cada dia. É um sentimento profundo, sincero e puro, e estou ansioso para compartilhar o meu futuro ao seu lado. Sei que a distância pode ser desafiadora, mas também sei que o amor que temos é forte o suficiente para superar qualquer obstáculo.
+
+                Obrigado, do fundo do meu coração, por ser a luz na minha vida. Obrigado por me fazer sentir amado e valorizado de uma maneira que nunca experimentei antes. Obrigado por ser você, por ser a Adriana maravilhosa que conheci e que agora preenche os meus dias com amor e alegria.
+
+                Estou ansioso para muitos mais momentos felizes juntos, para continuar a construir as nossas memórias e para um futuro brilhante ao seu lado. Você é o meu raio de sol, a minha inspiração e o meu amor verdadeiro.
+
+                Com todo o meu amor,<br/>
+
+                Milton Junior</p>
+        </div>
+
+    </div>
+
